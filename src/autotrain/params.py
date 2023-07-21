fe2552dc140db9140160923b926e0b4cd9ebbfd9
loganamcnichols@gmail.com <loganamcnichols@gmail.com>
2023-07-21 16:24:18 +0900

    Added training params for regression.

--- src/autotrain/params.py
+++ src/autotrain/params.py
@@ -264,6 +264,20 @@ class TextMultiClassClassificationParams(BaseModel):
     max_grad_norm: float = Field(1.0, title="Max gradient norm")
     seed: int = Field(42, title="Seed")
 
+class TextSingleColumnRegressionParams(BaseModel):
+    task: Literal["text_single_column_regression"]
+    learning_rate: float = Field(5e-5, title="Learning rate")
+    num_train_epochs: int = Field(3, title="Number of training epochs")
+    max_seq_length: int = Field(128, title="Max sequence length")
+    train_batch_size: int = Field(32, title="Training batch size")
+    warmup_ratio: float = Field(0.1, title="Warmup proportion")
+    gradient_accumulation_steps: int = Field(1, title="Gradient accumulation steps")
+    optimizer: str = Field("adamw_torch", title="Optimizer")
+    scheduler: str = Field("linear", title="Scheduler")
+    weight_decay: float = Field(0.0, title="Weight decay")
+    max_grad_norm: float = Field(1.0, title="Max gradient norm")
+    seed: int = Field(42, title="Seed")
+
 
 class DreamboothParams(BaseModel):
     task: Literal["dreambooth"]
